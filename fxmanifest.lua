a16e75cd6bda380e3f89a4d796072bb0fd671d07
kibukj <kibuzhj@gmail.com>
2021-09-10 10:03:54 -0400

    Add metadata to fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,11 @@
 fx_version "cerulean"
 game "common"
 
+name "discord_rest"
+author "kibukj"
+description "Discord REST API utility for FiveM and RedM"
+repository "https://github.com/kibook/discord_rest"
+
 server_scripts {
 	"config.lua",
 	"discord_rest.lua",
