8bc4549a3b6db9a3d213829a0405549b1bb22f5d
kibukj <kibuzhj@gmail.com>
2021-09-14 11:28:00 -0400

    Update example

--- examples/respond-to-message.lua
+++ examples/respond-to-message.lua
@@ -5,65 +5,57 @@ local channelIds = {} -- Channel to watch for command in
 
 -- Respond to a message
 local function respondToMessage(channel, message, content)
-        return exports.discord_rest:createMessage(channel.id, {
-                message_reference = {
-                        message_id = message.id
-                },
-                content = content
-        }, botToken)
+	return exports.discord_rest:createMessage(channel.id, {
+		message_reference = {
+			message_id = message.id
+		},
+		content = content
+	}, botToken)
 end
 
 -- Read messages in a watched channel, print them to console, and respond as necessary
 local function respondToMessagesInChannel(channel)
-        return exports.discord_rest:getChannelMessages(channel.id, {after = channel.last_message_id}, botToken):next(function(messages)
-                local promises = {}
+	return exports.discord_rest:getChannelMessages(channel.id, {after = channel.last_message_id}, botToken):next(function(messages)
+		if #messages == 0 then
+			return
+		end
 
-                if #messages == 0 then
-                        return
-                end
+		local promises = {}
 
-                for _, message in ipairs(messages) do
-                        print(("#%s: %s: %s"):format(channel.name, message.author.username, message.content))
+		for _, message in ipairs(messages) do
+			print(("#%s: %s: %s"):format(channel.name, message.author.username, message.content))
 
-                        if message.content == "!players" then
-                                table.insert(promises, respondToMessage(channel, message, "Players on server: " .. #GetPlayers()))
-                        end
-                end
+			if message.content == "!players" then
+				table.insert(promises, respondToMessage(channel, message, "Players on server: " .. #GetPlayers()))
+			end
+		end
 
-                channel.last_message_id = messages[#messages].id
+		channel.last_message_id = messages[#messages].id
 
-                return promise.all(promises)
-        end)
+		return promise.all(promises)
+	end)
 end
 
 -- Handle messages in all channels being watched
 local function respondToMessages(channels)
-        local promises = {}
-
-        for _, channel in ipairs(channels) do
-                table.insert(promises, respondToMessagesInChannel(channel))
-        end
-
-        return promise.all(promises)
+	return promise.map(channels, function(channel)
+		return respondToMessagesInChannel(channel)
+	end)
 end
 
 -- Get info for all channels being watched
 local function getChannels(channelIds)
-        local promises = {}
-
-        for _, channelId in ipairs(channelIds) do
-                table.insert(promises, exports.discord_rest:getChannel(channelId, botToken))
-        end
-
-        return promise.all(promises)
+	return promise.map(channelIds, function(channelId)
+		return exports.discord_rest:getChannel(channelId, botToken)
+	end)
 end
 
 getChannels(channelIds):next(function(channels)
-        Citizen.CreateThread(function()
-                while true do
-                        Citizen.Await(respondToMessages(channels))
-                end
-        end)
+	Citizen.CreateThread(function()
+		while true do
+			Citizen.Await(respondToMessages(channels))
+		end
+	end)
 end, function(err)
-        print("An error occured: " .. err)
+	print("An error occured: " .. err)
 end)
